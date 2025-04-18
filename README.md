0143d773824fa580f56a02ab213732fea2c7dfe0
Mandylaaane <ljungamanda@gmail.com>
2025-04-18 13:57:47 +0200

    Frontend: GA tag added

--- README.md
+++ README.md
@@ -17,12 +17,20 @@ TRACKING:
 
 SECURITY:
 
-- LOGGING & AUDIT:
+Mitigations:
+
+- REQUEST PARSING MIDDLEWARE:
+  Converts raw data into a usable JavaScript object, makes sure incoming requests are parsed safely and makes it easier to code.
+
+- ERRORS & UNHANDLED ROUTES:
+  Helps prevent app from crashing on unexpected errors and provides a consistent error response.
+
+- LOGGING & AUDIT: (morgan)
   With no action logging or audit, threats/attacks may go undetected.
 
-Mitigation:
+Not mitigated:
 
 - UNSECURE MONGODB:
   Having no authenication to my database is a threat, can lead to datatheft or unauthorized modifications.
 
-No mitigation created, but enabling authentication would help.
+Enabling authentication would prevent this threat.
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const createError = require("http-errors");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index.js");
 
@@ -16,20 +15,19 @@ mongoose.connection.on(
   console.error.bind(console, "MongoDB connection error:")
 );
 
-server.use(morgan("dev"));
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use(cookieParser());
+server.use(morgan("dev")); // LOGS ACCESS & ACTIONS
+server.use(express.json()); //
+server.use(express.urlencoded({ extended: false })); // PARSES RAW DATA SAFELY
 
 // MOUNT ROUTE
-server.use("/", indexRouter); // ---"/contacts" instead correct?!?!
+server.use("/", indexRouter);
 
 // CATCH 404 & SEND TO ERROR HANDLER
 server.use(function (req, res, next) {
   next(createError(404));
 });
 
-// ERROR HANDLER
+// ERROR HANDLER, FORMATS ERROR, PREVENTING CRASHES
 server.use(function (error, req, res) {
   res.status(error.status || 500);
   res.json({
